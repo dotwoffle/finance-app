b0e51b9272e9d37cdae5e84631c19b2d4f523583
Gbagecol <tannerlorenz1101@gmail.com>
2024-08-26 17:56:55 -0600

    Initial commit

---
+++ api_backend/src/test/java/com/dotwoffle/api/ApiWebServerTests.java
@@ -0,0 +1,13 @@
+package com.dotwoffle.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWebServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
