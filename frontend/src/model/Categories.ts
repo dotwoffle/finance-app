64e801f2c0dbe4b99a4155140bd691faba0e69b2
dotwoffle <tannerlorenz1101@gmail.com>
2024-09-11 20:52:50 -0600

    Added categories

---
+++ frontend/src/model/Categories.ts
@@ -0,0 +1,12 @@
+export enum ExpenseCategories {
+    FOOD = "food",
+    GROCERY = "grocery",
+    BILLS_AND_SUBSCRIPTIONS = "bills and subscriptions",
+    RENT = "rent",
+    OTHER = "other"
+}
+
+export enum IncomeCategories {
+    PAYCHECK = "paycheck",
+    OTHER = "other"
+}
