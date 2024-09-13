3b2310060dd105e9250b8fae1dcb41fe103a767d
dotwoffle <tannerlorenz1101@gmail.com>
2024-09-13 12:41:50 -0600

    Made records controller fetch records on load

--- frontend/src/components/RecordsController.tsx
+++ frontend/src/components/RecordsController.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import FinanceRecordCreator from "./FinanceRecordCreator";
 import FinanceRecordTable from "./FinanceRecordTable";
 import FinanceRecord from "../model/FinanceRecord";
+import Decimal from "decimal.js";
 
+interface ApiRecord {
+    uuid: string,
+    date: string,
+    description: string,
+    amount: number,
+    category: string
+};
+
+const API_ENDPOINT = "http://localhost:8080/api";
 
 export default function RecordsController(): JSX.Element {
 
@@ -17,6 +27,26 @@ export default function RecordsController(): JSX.Element {
         setTransactionList(updatedTransactionList);
 
     }
+
+    useEffect(() => {
+        fetch(`${API_ENDPOINT}/get-records`)
+                .then(response => response.json())
+                .then(responseBody => setTransactionList((responseBody as ApiRecord[]).map(record => {
+
+                        const [year, month, day] = record.date.split("-").map(Number);
+
+                        return {
+                            uuid: record.uuid,
+                            date: new Date(year, month-1, day),
+                            description: record.description,
+                            amount: new Decimal(record.amount),
+                            category: record.category
+                        };
+
+                    }))
+                )
+                .catch(error => console.error(`Failed to fetch records: ${error}`));
+    });
 
     return (
         <>
