66f7b2f3cdd874411461c39dcaf4b9f6d12131d0
Tanner Lorenz <54816222+dotwoffle@users.noreply.github.com>
2024-09-16 17:54:13 -0600

    Frontend dev (#6)
    * Initial commit
    
    * Switched to Vite setup; installed decimal.js
    
    * Added basic table component
    
    * Removed generated CSS
    
    * Created component to add transactions
    
    * Added UUID key to record type
    
    * Added remaining input fields
    
    * Fixed incorrect display values for date and amount
    
    * Feature/frontend api integration (#5)
    
    * Added controller to allow CORS headers
    
    * Made records controller fetch records on load
    
    * Added api call to create record

--- frontend/src/components/RecordsController.tsx
+++ frontend/src/components/RecordsController.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import FinanceRecordCreator from "./FinanceRecordCreator";
 import FinanceRecordTable from "./FinanceRecordTable";
 import FinanceRecord from "../model/FinanceRecord";
+import Decimal from "decimal.js";
 
+interface ApiRecord {
+    uuid: string,
+    date: string,
+    description: string,
+    amount: number,
+    category: string
+};
+
+const API_ENDPOINT = "http://localhost:8080/api";
 
 export default function RecordsController(): JSX.Element {
 
@@ -16,7 +26,45 @@ export default function RecordsController(): JSX.Element {
         updatedTransactionList.push(newTransaction);
         setTransactionList(updatedTransactionList);
 
+        fetch(`${API_ENDPOINT}/create-record`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                uuid: newTransaction.uuid,
+                date: newTransaction.date.toISOString(),
+                description: newTransaction.description,
+                amount: newTransaction.amount.toNumber(),
+                category: newTransaction.category,
+                type: "EXPENSE"
+            })
+        })
+        .catch(e => console.error(`Error occurred while posting record: ${e}`));
+
     }
+
+    useEffect(() => {
+        fetch(`${API_ENDPOINT}/get-records`)
+                .then(response => response.json())
+                .then(responseBody => setTransactionList((responseBody as ApiRecord[]).map(record => {
+
+                        const [year, month, day] = record.date.split("-").map(Number);
+
+                        return {
+                            uuid: record.uuid,
+                            date: new Date(year, month-1, day),
+                            description: record.description,
+                            amount: new Decimal(record.amount),
+                            category: record.category
+                        };
+
+                    }))
+                )
+                .catch(error => console.error(`Failed to fetch records: ${error}`));
+        },
+        []
+    );
 
     return (
         <>
