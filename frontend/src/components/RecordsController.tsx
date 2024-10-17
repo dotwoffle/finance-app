4d4e31600e5ea2946f3e3f26e8531a7706042777
dotwoffle <tannerlorenz1101@gmail.com>
2024-10-16 22:06:37 -0600

    Added date picker to fetch records by month/year

--- frontend/src/components/RecordsController.tsx
+++ frontend/src/components/RecordsController.tsx
@@ -21,6 +21,7 @@ const API_ENDPOINT = "http://localhost:8080/api";
 export default function RecordsController(): JSX.Element {
 
     const [transactionList, setTransactionList] = useState<FinanceRecord[]>([]);
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
     const handleFormSubmit: ((newTransaction: FinanceRecord) => void) = (newTransaction) => {
 
@@ -43,8 +44,51 @@ export default function RecordsController(): JSX.Element {
                 category: newTransaction.category,
                 type: newTransaction.type
             })
-        })
-        .catch(e => console.error(`Error occurred while posting record: ${e}`));
+        }).catch(e => console.error(`Error occurred while posting record: ${e}`));
+
+    }
+
+    const fetchRecordsForMonth: ((monthAndYear: Date | null) => void) = (monthAndYear) => {
+
+        console.log(`Fetching records for date: ${monthAndYear?.toISOString()}`);
+
+        let queryString: string | null = null;
+
+        if(monthAndYear !== null)
+        {
+            const minDate: Date = new Date(monthAndYear.getFullYear(), monthAndYear.getMonth(), 1);
+            const maxDate: Date = new Date(monthAndYear.getFullYear(), monthAndYear.getMonth() + 1, 0);
+
+            queryString = `q=date>=${minDate.toISOString().substring(0, 10)} date<=${maxDate.toISOString().substring(0, 10)}`;
+        }
+
+        console.log(`Query: ${queryString}`);
+
+        let endpoint: string = queryString !== null ?
+                `${API_ENDPOINT}/get-records?${queryString}` :
+                `${API_ENDPOINT}/get-records`;
+
+        fetch(endpoint)
+                .then(response => response.json())
+                .then(responseBody => setTransactionList((responseBody as ApiRecord[]).map(record => {
+
+                        const [year, month, day] = record.date.split("-").map(Number);
+
+                        return {
+                            uuid: record.uuid,
+                            date: new Date(year, month-1, day),
+                            type: TransactionType[record.type.toUpperCase() as keyof typeof TransactionType],
+                            description: record.description,
+                            amount: new Decimal(record.amount),
+                            category: TransactionCategory[record.category.toUpperCase() as keyof typeof TransactionCategory]
+                        };
+
+                    }))
+                )
+                // .then((responseBody) => {
+                //     console.log(`Response body: ${responseBody}`)
+                // })
+                .catch(error => console.error(`Failed to fetch records: ${error}`));
 
     }
 
@@ -71,13 +115,11 @@ export default function RecordsController(): JSX.Element {
         []
     );
 
-    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-
     return (
         <>
             <ReactDatePicker
                     selected={selectedDate}
-                    onChange={date => setSelectedDate(date)}
+                    onChange={date => {setSelectedDate(date); fetchRecordsForMonth(date);}}
                     dateFormat="MM/yyyy"
                     showMonthYearPicker
                     placeholderText="Select Month and Year"
