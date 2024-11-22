9efcbd5060db8c5e114c4f98c68240d654fbdac5
dotwoffle <tannerlorenz1101@gmail.com>
2024-11-22 12:15:08 -0700

    GET and POST SQL queries now work

--- api_backend/src/main/java/com/dotwoffle/api/web/ApiRestController.java
+++ api_backend/src/main/java/com/dotwoffle/api/web/ApiRestController.java
@@ -16,16 +16,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**The REST controller for the API backend. This class handles all API requests sent to available routes.*/
 @RestController
@@ -39,7 +43,11 @@ public class ApiRestController {
 
         DriverManager.registerDriver(new Driver());
 
-        this.DATABASE_CONNECTION = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
+        this.DATABASE_CONNECTION = DriverManager.getConnection(
+                "jdbc:mysql://localhost/finance_app",
+                "financeapp",
+                "FinanceAppDev12345!"
+        );
 
     }
 
@@ -76,18 +84,46 @@ public class ApiRestController {
 
     /**Generates a (optionally sorted) list of filtered finance records from the database.
      * @param queryString The query string passed into the request URL. This can be turned into a
-     * {@link com.dotwoffle.api.model.FinanceRecordQuery FinanceRecordQuery} object.
+     * {@link FinanceRecordQuery FinanceRecordQuery} object.
      * @return A list of all records from the database matching the query.*/
     @GetMapping("/api/get-records")
     private ResponseEntity<Collection<FinanceRecord>> getRecords(
             @RequestParam(name="q", required=false) String queryString
     ) throws MalformedQueryException {
 
-        if(queryString == null || queryString.isEmpty()) {
-            return ResponseEntity.ok(FAKE_DATABASE);
-        }
+        FinanceRecordQuery actualQuery = queryString == null ? null : FinanceRecordQuery.fromString(queryString);
 
-        return ResponseEntity.ok(applyQuery(FinanceRecordQuery.fromString(queryString)));
+        try(ResultSet queryResults = applyQuery(actualQuery)) {
+
+            Collection<FinanceRecord> recordList = new HashSet<>();
+
+            while(queryResults.next()) {
+
+                UUID id = UUID.fromString(queryResults.getString("recordId"));
+                FinanceRecord.Type type = FinanceRecord.Type.valueOf(queryResults.getString("type"));
+                BigDecimal amount = queryResults.getBigDecimal("amount");
+                LocalDate date = queryResults.getDate("date").toLocalDate();
+                String description = queryResults.getString("description");
+                FinanceRecord.Category category = FinanceRecord.Category.valueOf(queryResults.getString("category"));
+                FinanceRecord record = new FinanceRecord(
+                        id,
+                        type,
+                        amount,
+                        date,
+                        description,
+                        category
+                );
+
+                recordList.add(record);
+
+            }
+
+            return ResponseEntity.ok(recordList);
+
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -96,9 +132,21 @@ public class ApiRestController {
     @PostMapping("/api/create-record")
     private void postRecord(@RequestBody FinanceRecord financeRecord) {
 
+        System.out.println("Got record with id" + financeRecord.id());
+
         try {
-            Statement insertStatement = DATABASE_CONNECTION.createStatement();
-            insertStatement.execute("INSERT INTO test VALUES(13, 69);");
+
+            PreparedStatement insertStatement = DATABASE_CONNECTION.prepareStatement("INSERT INTO finance_records(recordId, type, category, description, amount, date) VALUES(?, ?, ?, ?, ?, ?)");
+
+            insertStatement.setString(1, financeRecord.id().toString());
+            insertStatement.setString(2, financeRecord.type().toString());
+            insertStatement.setString(3, financeRecord.category().toString());
+            insertStatement.setString(4, financeRecord.description());
+            insertStatement.setBigDecimal(5, financeRecord.amount());
+            insertStatement.setDate(6, Date.valueOf(financeRecord.date()));
+
+            insertStatement.execute();
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -108,31 +156,62 @@ public class ApiRestController {
 
     /**Applies a query to the database to return a filtered list of finance records.
      * @param query The query to apply.
-     * @return A filtered list of finance records matching the query.*/
-    private List<FinanceRecord> applyQuery(FinanceRecordQuery query) {
+     * @return The {@link ResultSet} object returned by the SQL query.*/
+    private ResultSet applyQuery(FinanceRecordQuery query) {
 
-        Stream<FinanceRecord> dbStream = FAKE_DATABASE.stream();
+        String selectStatementSql = "SELECT * FROM finance_records";
 
-        if(query.getLowerAmountBound() != null) {
-            dbStream = dbStream.filter(record -> record.amount().compareTo(query.getLowerAmountBound()) >= 0);
-        }
-        if(query.getUpperAmountBound() != null) {
-            dbStream = dbStream.filter(record -> record.amount().compareTo(query.getUpperAmountBound()) <= 0);
-        }
-        if(query.getLowerDateBound() != null) {
-            dbStream = dbStream.filter(record -> !record.date().isBefore(query.getLowerDateBound()));
-        }
-        if(query.getUpperDateBound() != null) {
-            dbStream = dbStream.filter(record -> !record.date().isAfter(query.getUpperDateBound()));
-        }
-        if(query.getAllowedTypes() != null) {
-            dbStream = dbStream.filter(record -> query.getAllowedTypes().contains(record.type()));
-        }
-        if(query.getAllowedCategories() != null) {
-            dbStream = dbStream.filter(record -> query.getAllowedCategories().contains(record.category()));
+        if(query != null) {
+
+            List<String> whereClauses = new ArrayList<>();
+
+            selectStatementSql += " WHERE ";
+
+            if(query.getLowerAmountBound() != null) {
+                whereClauses.add("amount >= " + query.getLowerAmountBound());
+            }
+            if(query.getUpperAmountBound() != null) {
+                whereClauses.add("amount <= " + query.getUpperAmountBound());
+            }
+            if(query.getLowerDateBound() != null) {
+                whereClauses.add("date >= '" + query.getLowerDateBound() + "'");
+            }
+            if(query.getUpperDateBound() != null) {
+                whereClauses.add("date <= '" + query.getUpperDateBound() + "'");
+            }
+            if(query.getAllowedTypes() != null) {
+
+                String typeListAsString = query.getAllowedTypes().stream()
+                        .map(FinanceRecord.Type::toString)
+                        .collect(Collectors.joining(","));
+
+                whereClauses.add("FIND_IN_SET(type, '" + typeListAsString + "')");
+
+            }
+            if(query.getAllowedCategories() != null) {
+
+                String categoryListAsString = query.getAllowedCategories().stream()
+                        .map(FinanceRecord.Category::toString)
+                        .collect(Collectors.joining(","));
+
+                whereClauses.add("FIND_IN_SET(type, '" + categoryListAsString + "')");
+
+            }
+
+            selectStatementSql += String.join(" AND ", whereClauses);
+
         }
 
-        return dbStream.collect(Collectors.toList());
+        System.out.println("Full query: " + selectStatementSql);
+
+        try {
+            Statement selectStatement = DATABASE_CONNECTION.createStatement();
+            selectStatement.execute(selectStatementSql);
+            return selectStatement.getResultSet();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
